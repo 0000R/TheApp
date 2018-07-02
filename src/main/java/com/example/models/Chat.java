8012907ebeb19f09bc6300c2285d33d3cd43818d
Roxanne Vasquez <r@Roxannes-MacBook-Pro.local>
2018-07-02 12:43:41 -0700

    initial commit 12:42pm Rox

---
+++ src/main/java/com/example/models/Chat.java
@@ -0,0 +1,10 @@
+package com.example.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="chat")
+public class Chat {
+
+}
---
+++ src/main/java/com/example/models/Home.java
@@ -0,0 +1,10 @@
+package com.example.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="home")
+public class Home {
+
+}
